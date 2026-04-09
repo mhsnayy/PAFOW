da71a0366d3c98aeefe80235d03ca2e0f1c8cf9e
ERBATAB MUHASEBE <ERBATABMUH01@erbatab.local>
2026-04-09 13:07:53 +0300

    config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output:'standalone',
-  allowedDevOrigins:['192.168.1.27','localhost:3000']
+  output: 'standalone',
+  allowedDevOrigins: ['192.168.1.27', 'localhost:3000']
 };
+
 
 export default nextConfig;
