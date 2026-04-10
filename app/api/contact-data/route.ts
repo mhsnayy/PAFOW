4c2ce024a3dd1496d86232c1e50c8d6a0bef1740
ERBATAB MUHASEBE <ERBATABMUH01@erbatab.local>
2026-04-10 16:10:05 +0300

    cors policy

---
+++ app/api/contact-data/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server';
+import { db } from '@/index';
+import { Contact, contacts } from '@/db/schema';
+
+
+
+
+export async function OPTIONS() {
+    return NextResponse.json({});
+}
+
+export async function POST(req: Request) {
+    try {
+        const body: Contact = await req.json();
+
+        //TODO: zod valid gelecek
+
+        const record = await db.insert(contacts).values({
+            fullName: body.fullName,
+            email: body.email,
+            phone: body.phone,
+            createdAt: body.createdAt,
+            message: body.message,
+            platform: body.platform
+        }).returning();
+
+        return NextResponse.json({ success: true, data: record }, { status: 201 });
+    } catch (error) {
+        console.error("DB Insert Error:", error);
+        return NextResponse.json({ error: 'Sunucu Hatası' }, { status: 500 });
+    }
+}
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,30 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  allowedDevOrigins: ['192.168.1.27', 'localhost:3000']
+  allowedDevOrigins: ['192.168.1.27', 'localhost:3000'],
+  async headers() {
+
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Credentials", value: "true"
+          },
+          {
+            key: "Access-Control-Allow-Origin", value: "https://doha-farma-v2.vercel.app"
+          },
+          {
+            key: "Access-Control-Allow-Methods", value: "POST"
+          },
+          {
+            key: "Access-Control-Allow-Origin", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Lenght, Content-MD5, Content-Type, Date, X-Api-Version"
+          }
+        ]
+      }
+    ]
+
+  }
 };
 
 
